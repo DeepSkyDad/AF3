448136a55de07a55872c707a1f451dc8bd9f1f78
Pavle Gartner <pavlinio2a@gmail.com>
2019-11-01 16:00:38 +0100

    control panel upgrades

--- DeepSkyDad.AF3.ControlPanel/Netz/RUN.bat
+++ DeepSkyDad.AF3.ControlPanel/Netz/RUN.bat
@@ -1,5 +1,5 @@
 netz -s -z -so ../bin/Release/DeepSkyDad.AF3.ControlPanel.exe ../bin/Release/CliWrap.dll ../bin/Release/Syroot.KnownFolders.dll
-xcopy "../bin/Release/Esptool" "../bin/Release/DeepSkyDad.AF3.ControlPanel.exe.netz/Esptool" /e /i /h
+xcopy "../bin/Release/Avrdude" "../bin/Release/DeepSkyDad.AF3.ControlPanel.exe.netz/Avrdude" /e /i /h
 cd "../bin/Release/DeepSkyDad.AF3.ControlPanel.exe.netz"
 del "AF3 Control Panel.exe"
 rename "DeepSkyDad.AF3.ControlPanel.exe" "AF3 Control Panel.exe"
